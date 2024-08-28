d95f1f4274b4b0265ca535f383c817fc20081447
Sebi Barta <sebibarta11@gmail.com>
2024-08-28 14:18:25 +0300

    fixmovement

---
+++ Assets/_Scripts/Units/Player/Actions/InputReader.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(menuName = "InputReader")]
+public class InputReader : ScriptableObject, GameInput.IGameplayActions, GameInput.IUIActions
+{
+
+    private GameInput _gameInput;
+
+    private void OnEnable()
+    {
+        if (_gameInput == null)
+        {
+            _gameInput = new GameInput();
+            _gameInput.Gameplay.SetCallbacks(this);
+            _gameInput.UI.SetCallbacks(this);
+
+            SetGameplay();
+        }
+
+    }
+
+    public void SetGameplay()
+    {
+        _gameInput.UI.Disable();
+        _gameInput.Gameplay.Enable();
+    }
+
+    public void SetUI()
+    {
+        _gameInput.Gameplay.Disable();
+        _gameInput.UI.Enable();
+    }
+
+    public event Action<Vector2> MoveEvent;
+    public event Action DashEvent;
+    public event Action NormalAttackEvent;
+    public event Action ChargedAttackEvent;
+
+    public event Action PauseEvent;
+    public event Action ResumeEvent;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnNormalAttack(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            NormalAttackEvent?.Invoke();
+        }
+    }
+
+    public void OnChargedAttack(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)    // in the future add on Start and Cancel phases
+        {
+            ChargedAttackEvent?.Invoke();
+        }
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            DashEvent?.Invoke();
+        }
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            PauseEvent?.Invoke();
+            SetUI();
+        }
+    }
+
+    public void OnResume(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            ResumeEvent?.Invoke();
+            SetGameplay();
+        }
+    }
+}
